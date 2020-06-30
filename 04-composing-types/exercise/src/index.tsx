6b1f17598642e63217cb1244a5a93d7233510c2f
Braedon Gough <braedon@pleo.io>
2020-06-30 21:59:08 +0200

    Less 4 solution

--- 04-composing-types/exercise/src/index.tsx
+++ 04-composing-types/exercise/src/index.tsx
@@ -1,17 +1,47 @@
 import React, { FC } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { CatFact, getRandomCatFacts } from './get-random-cat-facts'
 
+type Status = 'idle' | 'loading' | 'resolved' | 'error'
+interface Props {
+    catFacts: CatFact[]
+    status: Status
+}
+
+const CatFactsList = (props: Props) => {
+    switch (props.status) {
+        case 'idle':
+            return <div>no cat facts</div>
+        case 'loading':
+            return <div>Loading...</div>
+        case 'error':
+            return <div>Something went wrong</div>
+        case 'resolved':
+            return (
+                <ul>
+                    {props.catFacts.map((catFact) => (
+                        <li key={catFact._id}>{catFact.text}</li>
+                    ))}
+                </ul>
+            )
+    }
+}
+
 const App: FC = () => {
     const [catFacts, setCatFacts] = React.useState<CatFact[]>([])
-
+    const [status, setStatus] = React.useState<Status>('idle')
     React.useEffect(() => {
         const getFiveCatFacts = async () => {
             try {
+                setStatus('loading')
                 const facts = await getRandomCatFacts(5)
                 setCatFacts(facts)
-            } catch {}
+                setStatus('resolved')
+            } catch {
+                setStatus('error')
+            }
         }
 
         getFiveCatFacts()
@@ -20,7 +50,7 @@ const App: FC = () => {
     return (
         <>
             <h1>Best Facts about Cats!</h1>
-            <div>To be replaced with CatFactsList</div>
+            <CatFactsList status={status} catFacts={catFacts} />
         </>
     )
 }
